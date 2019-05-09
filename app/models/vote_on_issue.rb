2641afa5c0af1cd7f0674a133ddf084167627f37
Utopism <utopism@gmail.com>
2019-05-10 00:04:19 +0200

    unloadable removed (causes issues with hot reload), exception removed
    modifié :         app/models/vote_on_issue.rb

--- app/models/vote_on_issue.rb
+++ app/models/vote_on_issue.rb
@@ -1,25 +1,21 @@
 class VoteOnIssue < ActiveRecord::Base
-  unloadable
-  
   # Every vote belongs to a user and an issue
   belongs_to :user
   belongs_to :issue
-  
+
   def self.getMyVote(issue)
     iRet = 0
     begin
-     @vote = VoteOnIssue.find_by!("issue_id = ? AND user_id = ?", issue.id, User.current.id)
-     iRet = @vote.vote_val
-    rescue ActiveRecord::RecordNotFound
-     iRet = 0
+      @vote = VoteOnIssue.where(issue_id: issue.id).where(user_id: User.current.id).first
+      iRet = @vote.vote_val if @vote
     end
     iRet
   end
-  
+
   def self.getUpVoteCountOnIssue(issue_id)
     where("issue_id = ? AND vote_val > 0", issue_id).count
   end
-  
+
   def self.getDnVoteCountOnIssue(issue_id)
     where("issue_id = ? AND vote_val < 0", issue_id).count
   end
@@ -30,15 +26,14 @@ class VoteOnIssue < ActiveRecord::Base
     # this does load the users, less costly: One query for all users
       # includes(:user).where("issue_id = ? AND vote_val > 0", issue_id)
       # where("issue_id = ? AND vote_val > 0", issue_id).includes(:user)
-    # joins users successfully, but still execs one query for each user 
+    # joins users successfully, but still execs one query for each user
       # where("issue_id = ? AND vote_val > 0", issue_id).joins(:user)
     # This does what I want, but I'd love to find out how to do this in rails...
     find_by_sql( ["SELECT `vote_on_issues`.`vote_val` AS vote_val, `users`.`login` AS user_login FROM `vote_on_issues` LEFT JOIN `users` ON (`users`.`id` = `vote_on_issues`.`user_id`) WHERE (`issue_id` = ? AND `vote_val` > 0) ORDER BY user_login ASC", issue_id] )
   end
-  
+
   def self.getListOfDnVotersOnIssue(issue_id)
     # see getListOfUpVotersOnIssue
     find_by_sql( ["SELECT `vote_on_issues`.`vote_val` AS vote_val, `users`.`login` AS user_login FROM `vote_on_issues` LEFT JOIN `users` ON (`users`.`id` = `vote_on_issues`.`user_id`) WHERE (`issue_id` = ? AND `vote_val` < 0) ORDER BY user_login ASC", issue_id] )
   end
-  
 end
