57530a2407a36223ce998a76ae7ba72c7ffeca28
Utopism <utopism@gmail.com>
2019-05-10 00:02:11 +0200

    +sum_votes_up_and_dn, and where conditions normalized
    modifié :         lib/vote_on_issues/patches/query_patch.rb

--- lib/vote_on_issues/patches/query_patch.rb
+++ lib/vote_on_issues/patches/query_patch.rb
@@ -2,10 +2,10 @@
 module VoteOnIssues
   module Patches
     module QueryPatch
-      
+
       def sum_votes_up(issue)
         if User.current.allowed_to?(:view_votes, nil, :global => true)
-          VoteOnIssue.where('vote_val > 0 AND issue_id=?', issue.id).sum('vote_val')
+          VoteOnIssue.where(issue_id: issue.id).where('vote_val > 0').sum('vote_val')
         else
           '-'
         end
@@ -13,12 +13,19 @@ module VoteOnIssues
 
       def sum_votes_dn(issue)
         if User.current.allowed_to?(:view_votes, nil, :global => true)
-          VoteOnIssue.where('vote_val < 0 AND issue_id=?', issue.id).sum('vote_val')
+          VoteOnIssue.where(issue_id: issue.id).where('vote_val < 0').sum('vote_val')
         else
           '-'
         end
       end
-      
+
+      def sum_votes_up_and_dn(issue)
+        if User.current.allowed_to?(:view_votes, nil, :global => true)
+          VoteOnIssue.where(issue_id: issue.id).sum('vote_val')
+        else
+          '-'
+        end
+      end
     end
   end
 end
